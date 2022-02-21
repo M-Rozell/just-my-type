59fcd1adb9ff273d8b9e848224a374696d51c6f4
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-02-21 17:47:10 -0600

    highlighted buttons and appended the sentences.

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-$(document).ready(function () {
+let sentences = [
+  'I did not like the jibberish',
+  'So I changed it',
+  'I also wanted shorter sentances',
+  'Hope that is ok',
+  'It is changed now so...'];
+let sentanceCounter = 0;
+let letterCounter = 0;
 
-  $('#keyboard-upper-container').hide();
-  $(document).keydown(function (e) {
-    if (e.which === 16) {
-      $("#keyboard-lower-container").hide();
-      $('#keyboard-upper-container').show();
-    }
-  })
+$('#keyboard-upper-container').hide();
 
-  $(document).keyup(function (e) {
-    if (e.which === 16) {
-      $("#keyboard-lower-container").show();
-      $('#keyboard-upper-container').hide();
-    }
-  })
-
-  $(document).keypress(function (e) {
-    let keyCode = e.key.charCodeAt()
-    $('#' + keyCode).css('backgroundColor', 'gray');
-  })
-
-
-
-
+$(document).keydown(function (e) {
+  if (e.which === 16) {
+    $("#keyboard-lower-container").hide();
+    $('#keyboard-upper-container').show();
+  }
 })
+
+$(document).keyup(function (e) {
+  if (e.which === 16) {
+    $("#keyboard-lower-container").show();
+    $('#keyboard-upper-container').hide();
+  }
+})
+
+$(document).keypress(function (e) {
+  let keyCode = e.key.charCodeAt();
+  $('#' + keyCode).addClass('highlight')
+})
+
+$(document).keyup(function (e) {
+  let keyCode = e.key.charCodeAt();
+  $('#' + keyCode).removeClass('highlight');
+});
+
+
+$('#sentence').append(sentences[sentanceCounter]);
+
+
+
+// letterCounter++;
+// if (sentences[sentanceCounter].length == letterCounter.length) {
+//   sentanceCounter++;
+// }
+// $(document).keypress(function (e) {
+//   if (e.key.charCodeAt() === sentances[letterCounter].charCodeAt()) {
+//     $('#yellow-block').css('margin-left', '15px')
+//   }
+// })
+
+
+
+
 
